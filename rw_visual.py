d5900d449a1190fa9565050c80dc7038f6ce68ef
weizaikk <33487227+weizaikk@users.noreply.github.com>
2018-06-13 15:59:09 +0800

    Delete rw_visual.py

--- rw_visual.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from random_walk import Randomwalk
-
-rw = Randomwalk()
-rw.fill_walk()
-numwalks = list(range(rw.num_walks))
-plt.scatter(rw.x_values, rw.y_values, c=numwalks, cmap=plt.cm.Blues, s=15)
-plt.show()
