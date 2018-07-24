c4d62aba4e031763732b4ae59e03007d0ba17663
weizaikk <33487227+weizaikk@users.noreply.github.com>
2018-07-24 17:13:07 +0800

    抓取斗鱼房间弹幕并存储到MongoDB数据库，生成词云

--- douyu_danmu/111111111.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-room_id = input('请输入你想要接入的主播房间号')
-url = 'http://open.douyucdn.cn/api/RoomApi/room/{}'.format(room_id)
-text = requests.get(url)
-res = text.json()
-print(res)
