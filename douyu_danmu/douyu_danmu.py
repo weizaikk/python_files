87c50e1173907c9a14e47da0020eec4117dd0035
weizaikk <33487227+weizaikk@users.noreply.github.com>
2018-07-26 15:53:34 +0800

    解决部分弹幕和昵称无法decode问题，完善词云

--- douyu_danmu/douyu_danmu.py
+++ douyu_danmu/douyu_danmu.py
@@ -1,4 +1,4 @@
-# 爬取添加内容，主播的开播详情，返回错误信息提示，进行数据分析，发弹幕人的等级分析,词云
+# 爬取添加内容，主播的开播详情，返回错误信息提示，进行数据分析，发弹幕人的等级分析,词云，弹幕发的最多的
 import socket
 import pymongo
 import re
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # 连接到MongoDB数据库
 client_mongo = pymongo.MongoClient('localhost', 27017)
 db = client_mongo.Douyu_danmu
-col_msg = db.text
+col_msg = db.YT
 
 # socket通讯建立
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,7 +54,7 @@ def get_zb_name(room_id):
     status_code = data['data']['room_status']
     if data['error'] == 0:
         zbname = data['data']['owner_name']
-        print('#####################欢迎来到{}的直播间####################'.format(zbname))
+        print('*********************欢迎来到{}的直播间*********************'.format(zbname))
         if status_code == 2:
             print('该主播现在未开播')
     elif data['error'] == 999:
@@ -70,41 +70,6 @@ def keeplive():
     time.sleep(15)
 
 
-def get_response(room_id):
-    pac_content1 = 'type@=loginreq/roomid@={}/\0'.format(str(room_id))
-    send_packet(pac_content1)
-    pac_content2 = 'type@=joingroup/rid@={}/gid@=-9999\0'.format(room_id)
-    send_packet(pac_content2)
-    get_zb_name(room_id)
-    while True:
-        data_pac = client.recv(1024)
-        danmu = danmu_pattern.findall(data_pac)
-        uid = uid_pattern.findall(data_pac)
-        nickname = nickname_pattern.findall(data_pac)
-        level = level_pattern.findall(data_pac)
-        if not level:
-            level = b'0'
-        if not data_pac:
-            break
-        else:
-            for i in range(0, len(danmu)):
-                try:
-                    text = {
-                        'danmu': danmu[0].decode(encoding='utf-8'),
-                        'sender_uid': uid[0].decode(encoding='utf-8'),
-                        'sender_nickname': nickname[0].decode(encoding='utf-8'),
-                        'sender_level': level[0].decode(encoding='utf-8'),
-                        'write_time': str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())),
-                    }
-                    print(text)
-                    # 避免在数据库中插入重复的内容
-                    if not col_msg.find_one({'danmu': text['danmu'], 'sender_uid': text['sender_uid']}):
-                        col_msg.insert(text)
-                        print('成功保存到MongoDB->Douyu_danmu->text')
-                except Exception as e:
-                    print(e)
-
-
 # 获取生成词云所需要的单词
 def get_words_freq(col_msg):
     results = col_msg.find()
@@ -113,7 +78,7 @@ def get_words_freq(col_msg):
     for result in results:
         barrage = result['danmu']
         words = jieba.cut(str(barrage), cut_all=False)
-        #将一些无用的符号去除
+        # 将一些无用的符号去除
         with open('filter.txt', encoding='utf-8') as f:
             lines = f.readlines()
             for line in lines:
@@ -144,19 +109,57 @@ def draw_wordcloud():
     plt.figure()
     plt.imshow(wc.recolor(color_func=image_colors))
     plt.axis('off')
-    wc.to_file('word_cloud.jpg')
+    wc.to_file('word_cloud1.jpg')
+
+
+def get_run(room_id):
+    pac_content1 = 'type@=loginreq/roomid@={}/\0'.format(str(room_id))
+    send_packet(pac_content1)
+    pac_content2 = 'type@=joingroup/rid@={}/gid@=-9999\0'.format(room_id)
+    send_packet(pac_content2)
+    get_zb_name(room_id)
+    flag = True
+    while flag:
+        data_pac = client.recv(1024)
+        danmu = danmu_pattern.findall(data_pac)
+        uid = uid_pattern.findall(data_pac)
+        nickname = nickname_pattern.findall(data_pac)
+        level = level_pattern.findall(data_pac)
+        if not level:
+            level = b'0'
+        if not data_pac:
+            break
+        else:
+            for i in range(0, len(danmu)):
+                try:
+                    YT = {
+                        'danmu': danmu[0].decode(encoding='utf-8'),
+                        'sender_uid': uid[0].decode(encoding='utf-8'),
+                        'sender_nickname': nickname[0].decode(encoding='utf-8'),
+                        'sender_level': level[0].decode(encoding='utf-8'),
+                        'write_time': str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())),
+                    }
+                    # 避免在数据库中插入重复的内容
+                    if not col_msg.find_one({'danmu': YT['danmu'], 'sender_uid': YT['sender_uid']}):
+                        col_msg.insert(YT)
+                        print(YT)
+                        print('成功保存到MongoDB->Douyu_danmu')
+                    if col_msg.find().count() >= 5000:
+                        print('已成功存储5000条弹幕！')
+                        flag = False
+                        print('生成词云中......')
+                        draw_wordcloud()
+                        print('成功生成词云！')
+                        break
+                except Exception as e:
+                    print(e)
 
 
 if __name__ == '__main__':
     room_id = input('请输入你想要连接的直播房间号:  ')
-    p1 = multiprocessing.Process(target=get_response, args=(room_id,))
+    p1 = multiprocessing.Process(target=get_run, args=(room_id,))
     p2 = multiprocessing.Process(target=keeplive)
     p1.start()
     p2.start()
-    flag = input('是否生成词云(Y/N)\n')
-    if flag == 'Y':
-        draw_wordcloud()
     
-
-
 
